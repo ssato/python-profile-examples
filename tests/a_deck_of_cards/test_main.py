9fa27cb89a4c01758fdc2f6ba5fc2f33b28e8444
Satoru SATOH <satoru.satoh@gmail.com>
2021-06-22 10:16:16 +0900

    fix: follow changes in profile_examples.a_deck_of_cards

--- tests/a_deck_of_cards/test_main.py
+++ tests/a_deck_of_cards/test_main.py
@@ -5,7 +5,8 @@ import re
 
 import pytest
 
-import profile_examples.a_deck_of_cards.main as TT
+import profile_examples.a_deck_of_cards.utils as TT
+import profile_examples.a_deck_of_cards.constants as C
 
 
 CURDIR = pathlib.Path(__file__).parent
@@ -14,7 +15,7 @@ CURDIR = pathlib.Path(__file__).parent
 @pytest.mark.parametrize(
     ('curdir',  'pattern', 'expected'),
     (
-     (TT.CURDIR, TT.MOD_FILENAME_RE, ['ng_1', 'ng_2', 'ok_1', 'ok_2']),
+     (C.CURDIR, C.MOD_FILENAME_RE, ['ng_1', 'ng_2', 'ok_1', 'ok_2', 'ok_3']),
      (CURDIR, re.compile(r'__init__.py'), ['__init__']),
     )
 )
