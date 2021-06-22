03b0c4243dd4fd1916b9cb29462714f7f43f71f0
Satoru SATOH <satoru.satoh@gmail.com>
2021-06-22 10:58:49 +0900

    enhancement: add line_profiler support and its example session log

--- src/profile_examples/a_deck_of_cards/main.py
+++ src/profile_examples/a_deck_of_cards/main.py
@@ -21,26 +21,32 @@ DEFAULT_TIMES = 10000
     default='frozenset'
 )
 @click.option(
-    '--profile', is_flag=True, help='Get profle instead of measuring times'
+    '--profiler', type=click.Choice(['time', 'cProfile', 'line_profiler']),
+    help='Get profile data using it'
 )
 @click.option(
     '--print-results', is_flag=True, help='Print out computed results also'
 )
 def show_cards(times=DEFAULT_TIMES, module='', collection_type='frozenset',
-               profile=False, print_results=False):
+               profiler='cProfile', print_results=False):
     """Show cards in various ways.
     """
     sns = utils.get_suites_and_numbers(collection_type)
-    show_fn = prof.show_with_profile if profile else prof.show_with_time
     if module:
         cards_fn = utils.load_from_py(module)
         print(f'## module: {module}')
-        show_fn(times, cards_fn, *sns, print_results=print_results)
+        prof.show(
+            times, cards_fn, *sns, profiler=profiler,
+            print_results=print_results
+        )
     else:
         for mod_name in utils.list_modules():
             cards_fn = utils.load_from_py(mod_name)
             print(f'## module: {mod_name}')
-            show_fn(times, cards_fn, *sns, print_results=print_results)
+            prof.show(
+                times, cards_fn, *sns, profiler=profiler,
+                print_results=print_results
+            )
 
 
 if __name__ == '__main__':
--- src/profile_examples/a_deck_of_cards/prof.py
+++ src/profile_examples/a_deck_of_cards/prof.py
@@ -4,6 +4,8 @@ import cProfile
 import datetime
 import timeit
 
+import line_profiler
+
 
 def show_with_time(times, cards_fn, suites, numbers, print_results=False):
     """A decorator to show reuslts and time elapsed.
@@ -30,10 +32,38 @@ def show_with_profile(times, cards_fn, suites, numbers, print_results=False):
     """A decorator to show reuslts and time elapsed.
     """
     prof = cProfile.Profile()
-    prof.enable()
     prof.runcall(run_fun_times, times, cards_fn, suites, numbers)
     prof.print_stats()
 
     cards = cards_fn(suites, numbers)
     if print_results:
         print(f'cards: {sorted(cards)!r}')
+
+
+def show_with_line_profiler(times, cards_fn, suites, numbers,
+                            print_results=False):
+    """A decorator to show reuslts and time elapsed.
+    """
+    prof = line_profiler.LineProfiler()
+    prof.add_function(cards_fn)
+    prof.runcall(run_fun_times, times, cards_fn, suites, numbers)
+    prof.print_stats()
+
+    cards = cards_fn(suites, numbers)
+    if print_results:
+        print(f'cards: {sorted(cards)!r}')
+
+
+PROF_DEFAULT = show_with_time
+PROF_MAP = dict(
+    cProfile=show_with_profile,
+    line_profiler=show_with_line_profiler,
+)
+
+
+def show(times, cards_fn, suites, numbers,
+         profiler=PROF_DEFAULT, print_results=False):
+    """A decorator to show reuslts and time elapsed.
+    """
+    show_fn = PROF_MAP.get(profiler, PROF_DEFAULT)
+    show_fn(times, cards_fn, suites, numbers, print_results=print_results)
